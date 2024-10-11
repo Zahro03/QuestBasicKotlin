44999a4b541746afef3fceffc530c36efd7a9e3e
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-10-11 20:14:46 +0700

    Map ini dideklarasikan menggunakan mapOf dan tidak bisa diubah setelah dibuat dan struktur data yang immutable, sehingga tidak dapat menambah, mengubah, atau menghapus elemen dari map tersebut setelah inisialisasi.

--- app/src/main/java/com/example/myapplication/Collections.kt
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -56,4 +56,13 @@ fun ContohSet() {
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
 
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam map mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+
 }
