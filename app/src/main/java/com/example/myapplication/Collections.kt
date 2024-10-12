e7b2dd2c203ed66f8f7c04e34ffbab82b35f19d9
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-10-12 19:17:32 +0700

    Class Motor -> Mendefinisikan kelas kosong yang dinamakan Motor

--- app/src/main/java/com/example/myapplication/Collections.kt
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -52,6 +52,37 @@ fun ContohSet() {
     println()
     println("=== Set ===")
 
+    // Set Read-Only
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    //Menambahkan data ke dalam set mutable
+    shape.add("Rectangle")
+    println(shape)
+
+    //Menghapus data dari set mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
+
+// Map adalah Kumpulan data yang disusun dalam pasangan key-value
+// Map bersifat unordered,artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Map bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf
+
+fun ContohMap() {
+
+    println()
+    println("=== Map ===")
     // Map Read-Only
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
@@ -64,5 +95,21 @@ fun ContohSet() {
     shape["Rectangle"] = 4
     println(shape)
 
+    // Menghapus data dari map mutable
+    shape.remove("Circle")
+    println(shape)
 
+    // Mengubah data di dalam map mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(readOnlyShape)
+}
+
+fun main() {
+    ContohList()
+    ContohSet()
+    ContohMap()
 }
